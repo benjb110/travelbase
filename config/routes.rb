70fd7d7e6732dcc6a6817386fea00588edcd045c
Ben Bronson <bbronson@chicagobooth.edu>
2017-06-03 09:16:12 -0500

    commit am 2

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
+get "/users", :controller => "users", :action => "index"
+      get "/users/:id", :controller => "users", :action => "show"
 
 end
