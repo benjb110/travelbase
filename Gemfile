3f091af8a98c0987a8fe9fd9d34f1e378ea56d03
Ben Bronson <bbronson@chicagobooth.edu>
2017-06-03 08:55:07 -0500

    Sat AM

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem "starter_generators", :git => "https://github.com/raghubetina/starter_genera
 
 gem 'devise'
 
+gem 'bcrypt', platforms: :ruby
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,10 @@
 class Country < ApplicationRecord
 
-
+has_many :native_users, :class_name => "User", :foreign_key => "country_born_id", :dependent => :destroy
+has_many :residing_users, :class_name => "User", :foreign_key => "country_residing_id", :dependent => :destroy
+has_many :visits, :dependent => :destroy
+validates :name, :presence => true
+validates :name, :uniqueness => true
 
 
 
